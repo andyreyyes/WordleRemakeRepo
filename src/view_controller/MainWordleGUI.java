88b421d14c498f7b4977d7ff1fef241fbca36318
lilbrainus <brian.huy123@gmail.com>
2023-10-31 10:26:33 -0700

    started gui

---
+++ src/view_controller/MainWordleGUI.java
@@ -0,0 +1,49 @@
+package view_controller;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainWordleGUI extends Application {
+
+	private BorderPane pane;
+
+	private MenuBar menuBar;
+	private Menu home;
+	private Menu stats;
+	private Menu settings;
+	private Menu login;
+
+	public static void main(String args[]) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		layoutGUI();
+
+		Scene scene = new Scene(pane, 800, 800);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	private void layoutGUI() {
+		pane = new BorderPane();
+		menuBar = new MenuBar();
+		
+		home = new Menu("Home");
+		stats = new Menu("Stats");
+		settings = new Menu("Settings");
+		login = new Menu("Account");
+		
+		menuBar.getMenus().addAll(home, stats, settings, login);
+		pane.setTop(menuBar);
+		
+	}
+
+}
