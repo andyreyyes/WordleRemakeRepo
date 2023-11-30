2a092c1bdd785bff276bcbb4c0c384db33a9c772
andyreyes <andrew.reyes52012@gmail.com>
2023-11-29 20:32:56 -0700

    bug fix

--- src/view_controller/WordleGamePane.java
+++ src/view_controller/WordleGamePane.java
@@ -206,10 +206,12 @@ public class WordleGamePane extends TilePane {
 		} else if (letter.equals("")) {
 			return;
 		} else if (Character.isLetter(letter.charAt(0))) { // changes a character with what key was pressed
-			Button currentButton = grid[currentRow][currentCol];
-			currentButton.setText(letter.toUpperCase());
-			currentCol++;
-			animateButtonClick(currentButton);
+			if (currentRow < 6) {
+				Button currentButton = grid[currentRow][currentCol];
+				currentButton.setText(letter.toUpperCase());
+				currentCol++;
+				animateButtonClick(currentButton);
+			}
 		} else {
 			return;
 		}
